316c0a9e4ae3fd6851ebefc9f68d8d93097047b8
Denis Zolotarev <judaeus@gmail.com>
2024-02-29 18:54:02 +0200

    * init project

---
+++ src/main/java/ru/dzmakats/springmvcdemoapp/SpringMvcDemoappApplication.java
@@ -0,0 +1,13 @@
+package ru.dzmakats.springmvcdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcDemoappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcDemoappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/dzmakats/springmvcdemoapp/SpringMvcDemoappApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dzmakats.springmvcdemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcDemoappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
